3343d221c7930cafe4855c4a426f46706c82d7e8
имя <vishohirev@yandex.ru>
2019-05-15 14:23:31 +0500

    add brain-even

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [![Test Coverage](https://api.codeclimate.com/v1/badges/a4fa75e1ce42edae243e/test_coverage)](https://codeclimate.com/github/shohirev/project-lvl1-s486/test_coverage)
 
 [![Build Status](https://travis-ci.com/shohirev/project-lvl1-s486.svg?branch=master)](https://travis-ci.com/shohirev/project-lvl1-s486)
+
+[![asciicast](https://asciinema.org/a/IojvGhkP7qnjWIjthoKpMZ4sU.svg)](https://asciinema.org/a/IojvGhkP7qnjWIjthoKpMZ4sU)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  return (`Hello, ${userName}!`);
-};
+// нахождение случайного числа от 1 до 100
+const getRandom = () => Math.floor(Math.random() * 99 + 1);
 
-export default greeting;
+// игра
+const game = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  let log = 'Correct!';
+  let i = 0;
+  let number;
+  let answer;
+  while (log === 'Correct!' && i < 3) {
+    number = getRandom();
+    console.log(`Question: ${number}`);
+    answer = readlineSync.question('Your answer: ');
+    i += 1;
+    if (number % 2 === 0 && answer === 'yes') {
+      log = 'Correct!';
+    } else if (number % 2 !== 0 && answer === 'no') {
+      log = 'Correct!';
+    } else if (number % 2 !== 0 && answer === 'yes') {
+      log = `${answer} is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName}!`;
+    } else if (number % 2 === 0 && answer === 'no') {
+      log = `${answer} is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${userName}!`;
+    }
+    console.log(log);
+  }
+  if (log === 'Correct!') {
+    console.log(`Congratulations, ${userName}`);
+  }
+};
+export default game;
