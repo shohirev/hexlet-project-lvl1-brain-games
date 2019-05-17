4fa60c673683431c28d12663b44c515a68eff5b4
имя <vishohirev@yandex.ru>
2019-05-17 18:52:53 +0500

    add game is prime

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 [![Build Status](https://travis-ci.com/shohirev/project-lvl1-s486.svg?branch=master)](https://travis-ci.com/shohirev/project-lvl1-s486)
 
+[![asciicast](https://asciinema.org/a/uDnQJzSGrHQhpOUYafIlqfNAq.svg)](https://asciinema.org/a/uDnQJzSGrHQhpOUYafIlqfNAq)
+
+[![asciicast](https://asciinema.org/a/wQbByKcx11JTFF5zvsDiggzla.svg)](https://asciinema.org/a/wQbByKcx11JTFF5zvsDiggzla)
+
 [![asciicast](https://asciinema.org/a/l39FSACOjPMBU9fYK8NtZFFaj.svg)](https://asciinema.org/a/l39FSACOjPMBU9fYK8NtZFFaj)
 
 [![asciicast](https://asciinema.org/a/mZqQ7sVvBr5hb1NjvDsnC0ng4.svg)](https://asciinema.org/a/mZqQ7sVvBr5hb1NjvDsnC0ng4)
---
+++ src/games/prime.js
@@ -0,0 +1,41 @@
+import readlineSync from 'readline-sync';
+
+const getRandom = (min, max) => Math.floor(Math.random() * max + min);
+
+const isPrime = (number) => {
+  if (number < 4) {
+    return true;
+  }
+  let divisor = number - 1;
+  for (let i = number - 1; i > 1; i -= 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const greeting = () => console.log('Welcome to the Brain Games!');
+
+const showRule = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
+const startRound = () => {
+  const number = getRandom(1, 100);
+  console.log(`Question: ${number}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const correctAnswer = isPrime(number) === true ? "yes" : "no";
+  if (userAnswer === correctAnswer) {
+    return 'Correct!';
+  }
+  return `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again,`;
+};
+
+export {
+  greeting, showRule, getUserName, startRound,
+};
